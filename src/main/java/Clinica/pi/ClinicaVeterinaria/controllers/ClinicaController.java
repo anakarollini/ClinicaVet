91be1f2a7410f0ef29824e2e831972217c794fa6
Ana Karollini de Melo Silva <20211204110010@ifrn.local>
2023-01-23 16:17:04 -0300

    Criando Repository para Clinica

--- src/main/java/Clinica/pi/ClinicaVeterinaria/controllers/ClinicaController.java
+++ src/main/java/Clinica/pi/ClinicaVeterinaria/controllers/ClinicaController.java
@@ -1,13 +1,18 @@
 package Clinica.pi.ClinicaVeterinaria.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Clinica.pi.ClinicaVeterinaria.models.Clinica;
+import Clinica.pi.ClinicaVeterinaria.repositories.ClinicaRepository;
 
 @Controller
 public class ClinicaController {
+	
+	@Autowired
+	private ClinicaRepository cr;
 	
 	@RequestMapping("/clinica/form")
 	public String form() {
@@ -18,8 +23,9 @@ public class ClinicaController {
 	public String adicionar(Clinica clinica) {
     
     	System.out.println(clinica);
-    	
-    	
+    	cr.save(clinica);
+ 
+    
     	
 		return "veterinario-adicionado";
 	}
---
+++ src/main/java/Clinica/pi/ClinicaVeterinaria/repositories/ClinicaRepository.java
@@ -0,0 +1,10 @@
+package Clinica.pi.ClinicaVeterinaria.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Clinica.pi.ClinicaVeterinaria.models.Clinica;
+
+public interface ClinicaRepository extends JpaRepository<Clinica, Long>{
+	
+
+}
